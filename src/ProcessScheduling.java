da729414a6575843c87301200b7ca0a4faa20f9d
Thinkman Wang <wangxf1985@gmail.com>
2021-11-22 01:02:43 +0800

    update

--- src/ProcessScheduling.java
+++ src/ProcessScheduling.java
@@ -1,5 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintStream;
 import java.util.*;
 
 class ProcessInfo {
@@ -26,12 +28,12 @@ class ProcessInfo {
         return szRet;
     }
 
-    public String toStringFinissh() {
-        String szRet = String.format("Process id = %d\n\tpriority = %d\n\tduration = %d\n\tarrival time = %d\n"
+    public String toStringFinish() {
+        String szRet = String.format("Process id = %d\n\tPriority = %d\n\tArrival = %d\n\tDuration = %d\n"
                 , this.getmPID()
                 , this.getmPriority()
-                , this.getmDuration()
-                , this.getmAarrivalTime());
+                , this.getmAarrivalTime()
+                , this.getmDuration());
 
         return szRet;
     }
@@ -83,6 +85,9 @@ class ProcessScheduler {
     private boolean mAllFinished = false;
     public static final int MAX_WAIT_TIME = 30;
     public static int mTotalWaitTime = 0;
+    public static int mTotalProcess = 0;
+    private String mOutFile = null;
+    private PrintStream mOutStream = null;
 
     private List<ProcessInfo> mProcessNotReady = new ArrayList<>();
     private PriorityQueue<ProcessInfo> mProcess = new PriorityQueue<>(new Comparator<ProcessInfo>() {
@@ -95,11 +100,20 @@ class ProcessScheduler {
     public void printAllProcess() {
         for (ProcessInfo item : mProcessNotReady) {
             System.out.println(item.toString());
+            mOutStream.println(item.toString());
         }
+    }
+
+    public void printMaxWait() {
+        System.out.printf("\nMaximum wait time = %d\n\n", ProcessScheduler.MAX_WAIT_TIME);
+        mOutStream.printf("\nMaximum wait time = %d\n\n", ProcessScheduler.MAX_WAIT_TIME);
     }
 
     private void updateQueue() {
         //Get (don’t remove) a process p from D that has the earliest arrival time
+        if (mProcessNotReady.isEmpty()) {
+            return;
+        }
 
         Iterator<ProcessInfo> iterator = mProcessNotReady.iterator();
         while(iterator.hasNext()) {
@@ -110,10 +124,16 @@ class ProcessScheduler {
                 mProcess.add(item);
             }
         }
+
+        if (mProcessNotReady.isEmpty()) {
+            System.out.printf("D becomes empty at time %d\n", mCurrentTime);
+            mOutStream.printf("D becomes empty at time %d\n", mCurrentTime);
+        }
     }
 
     private void updatePriority() {
         System.out.println("Update priority: ");
+        mOutStream.println("Update priority: ");
 
         for (ProcessInfo item : mProcess) {
             if (mCurProcess == item) {
@@ -131,19 +151,39 @@ class ProcessScheduler {
                         , item.getmPID()
                         , item.getmPriority() - 1);
 
+                mOutStream.printf("" +
+                                "PID = %d, wait time = %d, current priority = %d \n" +
+                                "PID = %d, new priority = %d\n"
+                        , item.getmPID()
+                        , mCurrentTime - item.getmAarrivalTime()
+                        , item.getmPriority()
+                        , item.getmPID()
+                        , item.getmPriority() - 1);
+
                 item.setmPriority(item.getmPriority() - 1);
             }
         }
 
         System.out.println();
+        mOutStream.println();
     }
 
-    public void init(String szFile) {
+    private void onFinish() {
+        mOutStream.close();
+    }
+
+    public void init(String szInputFile, String szOutFile) {
+        mOutFile = szOutFile;
+        try {
+            mOutStream = new PrintStream(mOutFile);
+        } catch (Exception ex) {
+        }
+
         FileReader fr = null;
         BufferedReader bf = null;
 
         try {
-            fr = new FileReader(szFile);
+            fr = new FileReader(szInputFile);
             bf = new BufferedReader(fr);
 
             String str;
@@ -157,6 +197,8 @@ class ProcessScheduler {
                         , Integer.parseInt(lstData[2])
                         , Integer.parseInt(lstData[3])
                 ));
+
+                mTotalProcess += 1;
             }
         } catch (Exception ex) {
 
@@ -172,6 +214,10 @@ class ProcessScheduler {
     }
 
     private void createProcess() {
+        if (mProcess.isEmpty()) {
+            return;
+        }
+
         mCurProcess = mProcess.remove();
         mTotalWaitTime += mCurrentTime - mCurProcess.getmAarrivalTime();
         System.out.printf("Process removed from queue is: id = %d, at time %d, wait time = %d Total wait time = %.1f\n"
@@ -179,9 +225,15 @@ class ProcessScheduler {
                 , mCurrentTime
                 , mCurrentTime - mCurProcess.getmAarrivalTime()
                 , (float)mTotalWaitTime);
+        mOutStream.printf("Process removed from queue is: id = %d, at time %d, wait time = %d Total wait time = %.1f\n"
+                , mCurProcess.getmPID()
+                , mCurrentTime
+                , mCurrentTime - mCurProcess.getmAarrivalTime()
+                , (float)mTotalWaitTime);
     }
 
     public void start() {
+
         while (false == mAllFinished) {
             try {
 
@@ -197,11 +249,17 @@ class ProcessScheduler {
 
                     if (mCurProcess.getmRunTime() >= mCurProcess.getmDuration()) {
                         //process finished
-                        System.out.println(mCurProcess.toStringFinissh());
+                        System.out.println(mCurProcess.toStringFinish());
+                        mOutStream.println(mCurProcess.toStringFinish());
+
                         System.out.printf("Process %d finished at time %d\n\n", mCurProcess.getmPID(), mCurrentTime);
+                        mOutStream.printf("Process %d finished at time %d\n\n", mCurProcess.getmPID(), mCurrentTime);
+
+                        mCurProcess = null;
 
                         updatePriority();
                         createProcess();
+
                     } else {
                         continue;
                     }
@@ -225,16 +283,25 @@ class ProcessScheduler {
                 }
             }
         }
+
+        /*
+        Total wait time = 391.0 Average wait time = 39.1
+         */
+        System.out.printf("\nTotal wait time = %.1f \nAverage wait time = %.1f\n", (float)mTotalWaitTime, ((float)mTotalWaitTime/(float)mTotalProcess));
+        mOutStream.printf("\nTotal wait time = %.1f \nAverage wait time = %.1f\n", (float)mTotalWaitTime, ((float)mTotalWaitTime/(float)mTotalProcess));
+
+        this.onFinish();
     }
 }
 
 public class ProcessScheduling {
     public static void main(String[] args) {
         ProcessScheduler processScheduler = new ProcessScheduler();
-        processScheduler.init("/Users/wangxiaofeng/Github-Thinkman/Assignment/src/process_scheduling_input.txt");
+        processScheduler.init("/Users/wangxiaofeng/Github-Thinkman/Assignment/src/process_scheduling_input.txt"
+                , "/Users/wangxiaofeng/Github-Thinkman/Assignment/src/process_scheduling_output.txt");
         processScheduler.printAllProcess();
+        processScheduler.printMaxWait();
 
-        System.out.printf("\nMaximum wait time = %d\n\n", ProcessScheduler.MAX_WAIT_TIME);
         processScheduler.start();
     }
 }
